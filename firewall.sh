cafa698fd53f589468700793876a9ce0ff6d222d
Joel Fernandes <36451882+JoelFernandes01@users.noreply.github.com>
2024-12-29 18:11:18 -0300

    firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-USER=$1
+# Variáveis de ambiente
+USER="$1"
 $USER
+SSH="2223"
 
 echo "Esse script foi desenvolvido para ambientes linux usando distribuições Debian ou Ubuntu"
 echo "Atualizando pacotes do servidor e instalando o UFW (Uncomplicated Firewall)"
@@ -15,7 +17,7 @@ sudo systemctl daemon-reload && sudo systemctl enable --now rsyslog
 
 echo "Criando um novo usuário e incluindo ele no grupo sudo"
 sudo adduser $USER --gecos "Nome Completo,TelefoneCelular" --disabled-password
-echo "Adicionando $user ao grupo sudo"
+echo "Adicionando $USER ao grupo sudo"
 sudo usermod -aG sudo $USER
 
 echo "Desativando login do usuário root via SSH"
@@ -26,7 +28,8 @@ sudo sed -i 's/X11Forwarding yes/X11Forwarding no/' /etc/ssh/sshd_config
 sudo sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/' /etc/ssh/sshd_config
 sudo sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding no/' /etc/ssh/sshd_config
 sudo sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/' /etc/ssh/sshd_config
-sudo sed -i 's/#Port 22/Port 2223/' /etc/ssh/sshd_config
+sudo sed -i "s/#Port 22$/Port $SSH/" /etc/ssh/sshd_config
+#sudo sed -i "s/^Port 22$/Port $SSH/" /etc/ssh/sshd_config
 
 echo "Ativando o Uncomplicated Firewall"
 sudo ufw --force enable
@@ -39,7 +42,7 @@ sudo ufw logging medium
 echo "Configurando regras no UFW"
 ufw allow in on lo comment 'Liberando comunicação na interface loopbak'
 ufw allow out on lo comment 'Liberando comunicação na interface loopbak'
-sudo ufw allow 2223/tcp comment 'Liberando a porta de acesso SSH'
+sudo ufw allow $SSH/tcp comment 'Liberando a porta de acesso SSH'
 sudo ufw allow out 53/udp comment 'Liberando a saida para consulta do DNS'
 sudo ufw allow out 123/udp comment 'Liberando a saida para sincronismo do NTP'
 
